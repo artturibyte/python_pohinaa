3e90061c84013c2a1484b51396a80b1e1a5d562e
Artturi Mard <artturi.mard@novatron.fi>
2024-09-30 17:53:11 +0300

    typer cli tool

--- hello.py
+++ hello.py
@@ -1,9 +1,17 @@
 import numpy as np
+import typer
 
-def main():
-    print("Hello from python-pohinaa!")
+
+typer_app = typer.Typer(add_completion=False)
+
+@typer_app.command()
+def greetings(name: str):
+    print(f"Hello from python-pohinaa {name}!")
+
+@typer_app.command()
+def get_pi():
     print(np.pi)
 
 
 if __name__ == "__main__":
-    main()
+    typer_app()
